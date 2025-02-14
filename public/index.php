d35269b6ce02039fe83e4fe5cfc109c36f8f5945
wiktorstacel <stawik964@gmail.com>
2025-02-14 17:43:02 +0100

    Moving index.php to new public folder

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
  * and open the template in the editor.
  */
 
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 use app\core\Application;
 
 $app = new Application();
