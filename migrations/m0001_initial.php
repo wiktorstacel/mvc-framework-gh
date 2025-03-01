0ece308de7a3116da13b86b4698019aeb2b306ba
wiktorstacel <stawik964@gmail.com>
2025-03-01 20:16:42 +0100

    Migrations - making connection to database using .env

---
+++ migrations/m0001_initial.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\migrations;
+
+class m0001_initial
+{
+    public function up()
+    {
+        echo "Applying migration.";
+    }
+
+    public function down()
+    {
+        echo "Down migration.";
+    }
+    
+}
---
+++ migrations/m0002_something.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\migrations;
+
+class m0002_something
+{
+    public function up()
+    {
+        echo "Applying migration.";
+    }
+
+    public function down()
+    {
+        echo "Down migration.";
+    }
+}
